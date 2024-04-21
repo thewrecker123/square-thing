84aa891ff0ca39abdee6d534c3073c0076d73b5a
thewrecker123 <105166080+thewrecker123@users.noreply.github.com>
2024-04-21 12:38:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, 100);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results){
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX +" noseY = " + noseY);
+ 
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
+
+    }
+}
